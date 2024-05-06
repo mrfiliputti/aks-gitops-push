fc5c10ed454a72705415b4371b828974410455f8
Fernando C Filiputti <fernando.cf@live.com>
2024-05-06 17:21:54 -0300

    First Version

---
+++ src/ffwebapp-test/IndexPageTests.cs
@@ -0,0 +1,11 @@
+namespace ffwebapp.tests;
+
+[TestClass]
+public class IndexPageTests
+{
+    [TestMethod]
+    public void OnGet_ExecutesSuccessfully()
+    {
+        Assert.IsTrue(true);
+    }         
+}
